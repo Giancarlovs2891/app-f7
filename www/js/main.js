53ae29350656f058a6d147f1f004215d3267729a
giancarlovs2891 <giancarlovs2891@gmail.com>
2017-08-31 09:07:03 -0500

    Sync data view added

--- www/js/main.js
+++ www/js/main.js
@@ -1,15 +1,10 @@
-document.addEventListener("backbutton", function(e){
-    myApp.alert("Backbutton");
-}, false);
-
-
-
-
 function addProductToCart(x) {
   myApp.prompt('Cantidad', function (data) {
       // @data contains input value
-      myApp.confirm('Are you sure that your name is ' + data + '?', function () {
-          myApp.alert('Ok, your name is ' + data + ' ;)');
-      });
+      // myApp.confirm('Are you sure that your name is ' + data + '?', function () {
+      //   myApp.addNotification({
+      //       message: 'Producto agregado Cantidad:' + data
+      //   });
+      // });
   });
 }
